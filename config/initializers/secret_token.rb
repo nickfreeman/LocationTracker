9c9aca65f83395eb2c3e989d187f09db2281d652
nickfreeman <nickafreeman@berkeley.edu>
2013-10-19 17:17:17 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LocationTracker::Application.config.secret_token = '4b00a20ef6900e85b377651ca0451675c1e67a293f54862ae005bad9b7301a496846eeb25e4ebc72a9995a5ecf17dc9216e9a2b900736cac60196327bdc66d76'
