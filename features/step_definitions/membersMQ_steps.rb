848f4610b127bc8547a1450a0a21791897e8c496
saasbook <saasbook@saasbook.(none)>
2013-10-26 16:21:47 -0700

    MMQ view

--- features/step_definitions/membersMQ_steps.rb
+++ features/step_definitions/membersMQ_steps.rb
@@ -4,8 +4,16 @@ Given /the following member info exist/ do |member_table|
   end
 end
 
+Given /I am a logged in member/ do
+	#login feature not implemented yet
+end
+
 Then /I should see Successfully Sent/ do
      step %{I should see "Successfully Sent"}
+end
+
+Given /empty database/ do 
+  Member.destroy_all
 end
 
 Then /I should see No members available/ do
