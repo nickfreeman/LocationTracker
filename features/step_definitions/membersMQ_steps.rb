44f5a92907f2370020140d7692a0184f5e208564
saasbook <saasbook@saasbook.(none)>
2013-10-26 14:21:56 -0700

    member created

--- features/step_definitions/membersMQ_steps.rb
+++ features/step_definitions/membersMQ_steps.rb
@@ -1,14 +1,15 @@
-Given /the following member info exist/ do |emails_table|
-  emails_table.hashes.each do |email|
-    Email.create(email)
+Given /the following member info exist/ do |member_table|
+  emails_table.hashes.each do |member|
+    Member.create(member)
   end
 end
 
 Then /I should see Successfully Sent/ do
      step %{I should see "Successfully Sent"}
+end
 
 Then /I should see No members available/ do
     if Email.count == 0
       step %{I should see "No members available"}
     end
-
+end
