a426f90bf9c4efbc5f85f084094d43782ff80dfa
yinajin <yinajin@berkeley.edu>
2013-11-03 01:06:55 -0700

    something broke with email; rolls back

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -15,17 +15,25 @@ class MembersController < ApplicationController
     redirect_to members_path
   end
 
+	#def email
+		#@members = Member.all
+		#if @members.length == 2
+			#flash[:notice] =  "No members available."
+			#render "/index"
+		#else
+			#@members.each do |member|
+				#UserMailer.seek_email(member).deliver
+			#end
+			#redirect_to "/success"
+		#end
+	#end
+
 	def email
 		@members = Member.all
-		if @members.length == 2
-			flash[:notice] =  "No members available."
-			render "/index"
-		else
-			@members.each do |member|
-				UserMailer.seek_email(member).deliver
-			end
-			redirect_to "/success"
+		@members.each do |member|
+			UserMailer.seek_email(member).deliver
 		end
+		redirect_to "/members#index"
 	end
 
 end
