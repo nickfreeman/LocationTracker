49dd2b5264a035d82c18f15178d52018a0da490e
yinajin <yinajin@berkeley.edu>
2013-11-09 15:26:56 -0800

    add view when user is logged in

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 LocationTracker::Application.routes.draw do
   authenticated :user do
-    root :to => 'home#index'
+    root :to => 'home#show'
   end
   root :to => "home#index"
   devise_for :users
