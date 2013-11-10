2b1085f9d993b97771d96a86c2e9fb31bc580af2
saasbook <saasbook@saasbook.(none)>
2013-11-10 00:40:53 -0800

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#Member.create!({:name => 'Yina Jin', :email => 'solaris.jin@gmail.com', :role => #'goddess'})
 
-Member.create!({:name => 'Yina Jin', :email => 'solaris.jin@gmail.com', :role => 'goddess'})
-Member.create!({:name => 'Shaumik Mondal', :email => 'themaster_759@hotmail.com', :role => 'god'})
-
+User.create!({:name => 'Yeung John Li', :email => 'liyeungjohn@yahoo.ca', :password => 'liyeungjohn', :password_confirmation => 'liyeungjohn', :role => 'field', :remember_me => 'yes'})
+User.create!({:name => 'Yina Jin', :email => 'solaris.jin@gmail.com', :password => 'jinyina_password', :password_confirmation => 'jinyina_password', :role => 'field', :remember_me => 'yes'})
+User.create!({:name => 'Shaumik Mondal', :email => 'themaster_759@hotmail.com', :password => 'mondalshaumik', :password_confirmation => 'mondalshaumik', :role => 'AHC', :remember_me => 'yes'})
+User.create!({:name => 'James Kim', :email => 'james@email.com', :password => 'kimjames', :password_confirmation => 'kimjames', :role => 'AHC', :remember_me => 'yes'})
