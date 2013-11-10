fd65d353604ab9b09d691a3f0f6009c3535fb983
Shaumik <shaumik@Greed.(none)>
2013-11-10 10:39:07 -0800

    map

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-
-    function initialize(){
-	var mapOptions = {
-	    center: new google.maps.LatLn(-34.397, 150.644)//include variable long and lat
-	    zoom: 8,
-	    mapTypeId: google.maps.MaptTypeId.ROADMAP
-	};
-	var map = new google.maps.Map(document.getElementByID("map-canvas"), mapOptions);
+    function initialize() {
+        var mapOptions = {
+            center: new google.maps.LatLng(-34.397, 150.644),
+            zoom: 8,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+				      mapOptions);
     }
     google.maps.event.addDomListener(window, 'load', initialize);
 
