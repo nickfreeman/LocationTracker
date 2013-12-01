fc74ca364668a6508b01d5162696ed7e94adb080
yinajin <yinajin@berkeley.edu>
2013-12-01 11:47:42 -0800

    can now add tags to people

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,8 @@ LocationTracker::Application.routes.draw do
 
   resources :locations
 
+  resources :tags
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
   
