b3623d8f5a2b06b94cef7661a3d68a45d95c9bb3
Nick Freeman <nickafreeman@berkeley.edu>
2013-12-01 23:30:59 -0800

    Finished filtering!

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,9 @@
 class HomeController < ApplicationController
+
   def index
   end
 
   def show()
-    #puts "\n\n\n\n\n"
-    #puts params[:search_time]
 
     @autocomplete_people = User.all.map(&:name)
   	@locations = [[37.488902,-122.230657, 'HeadQuarter','']]
@@ -12,13 +11,23 @@ class HomeController < ApplicationController
     
 	  if Location.count != 0
       if params[:search_name] == nil or params[:search_name] == ""
-        
-    		@geolocations = Location.all
-        #@geolocations = Location.where(:created_at => params[:search_time].minutes.ago..Time.now)
+        if params[:search_time] == nil or params[:search_time] == "" or params[:search_time] == 0
+          session.delete(:search_time)
+          @geolocations = Location.all
+        else
+          session[:search_time] = params[:search_time]
+          @geolocations = Location.where(:updated_at => params[:search_time].to_i.minutes.ago..Time.now)
+        end
         
   	  else
-        
-        @geolocations = Location.where("name = ?", params[:search_name])
+
+        if session[:search_time] == nil or session[:search_time] == "" or session[:search_time] == 0
+          @geolocations = Location.where("name = ?", params[:search_name])
+        else
+          @geolocations = Location.where(:updated_at => session[:search_time].to_i.minutes.ago..Time.now).where("name = ?", params[:search_name])
+        end
+
+        #@geolocations = Location.where("name = ?", params[:search_name])
         @tracing = true
         
       end
